0e849fd6d8df4ee15873053b5a7266bd5202a07e
sehrishkhan336 <44490311+sehrishkhan336@users.noreply.github.com>
2023-08-20 14:33:43 -0700

    added the links and icons for github, linkedin and stackoverflow

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,23 +1,48 @@
-// Footer.js
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, IconButton } from '@mui/material';
+import { LinkedIn, GitHub } from '@mui/icons-material';
+import { FaStackOverflow } from 'react-icons/fa';
 
 const Footer = () => {
+  return (
+    <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0, height: '65px' }}>
+      <Toolbar sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        
+        <div>
+          <IconButton
+            aria-label="LinkedIn"
+            color="inherit"
+            href="https://www.linkedin.com/in/sehrish-khan-63056416/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedIn size={30} />
+          </IconButton>
+          
+          <IconButton
+            aria-label="GitHub"
+            color="inherit"
+            href="https://github.com/sehrishkhan336"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHub size={30} />
+          </IconButton>
+          
+          <IconButton
+            aria-label="Stack Overflow"
+            color="inherit"
+            href="https://stackoverflow.com/users/21168319/sehrish-khan"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+           <FaStackOverflow size={30} />
+          </IconButton>
+        </div>
 
-   
-    return (
-        <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0, height: '85px' }}>
-            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-
-                <div>
-                <Typography variant="body1" color="inherit" align="center">
-
-                </Typography>
-                </div>
-
-            </Toolbar>
-        </AppBar>
-    );
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Footer;
