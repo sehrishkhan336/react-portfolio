ebee85aa57345862624dbeb7c3922e474e2a4079
sehrishkhan336 <44490311+sehrishkhan336@users.noreply.github.com>
2023-08-21 13:57:56 -0700

    updated links and styling of portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import { Card, CardContent, CardMedia, Grid, Typography, Button } from '@mui/material';
 import { Container } from '@mui/system';
 import tictactoeimage from '../../images/tic-tac-toe.gif';
 import WeatherDashboardImage from '../../images/weather-dashboard.gif';
@@ -12,84 +12,111 @@ const Portfolio = () => {
     const projects = [
         {
             title: 'Tic Tac Toe',
-            description: 'This is a simple Tic Tac Toe game built with React. It was built as part of the React tutorial.',
+            description: 'React Application that allows the user to play tic tac toe.',
             imageSrc: tictactoeimage,
+            projectLink: 'https://github.com/sehrishkhan336/tic-tac-toe',
         },
         {
             title: 'Weather Dashboard',
             description:
                 'This is a weather dashboard that allows the user to search for a city and see the current weather as well as the 5 day forecast.',
             imageSrc: WeatherDashboardImage,
+            projectLink: 'https://github.com/sehrishkhan336/Weather-Dashboard/tree/master',
         },
         {
             title: 'Work Day Scheduler',
             description:
-                'This is a simple calendar application that allows the user to save events for each hour of the day. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.',
+                'This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.',
             imageSrc: WorkdaySchedulerImage,
+            projectLink: 'https://github.com/sehrishkhan336/Note-Taker-Aap',
         },
         {
             title: 'FineDine Review App',
             description:
                 'This Application is a review app. Google Apis are used to locate restaurants and the user can leave a review for the restaurant.',
-            imageSrc: FoodReviewAppImage, 
+            imageSrc: FoodReviewAppImage,
+            projectLink: 'https://findine.herokuapp.com/',
         },
         {
             title: 'Rythmix ConcertHub',
             description:
-                'This is a timed coding quiz with multiple-choice questions. This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript.',
+                'An application that allows the user to find concert happening in their area. The user can also search for a specific artist and see their upcoming concerts.',
             imageSrc: RythmixConcertHubImage,
+            projectLink: 'https://sehrishkhan336.github.io/Portfolio-Sehrish-Khan/',
         },
         {
             title: 'DocTalk',
             description:
                 'This is a React Application that allows the user to search for a doctor by name or specialty. The user can also chat, email and request a call back from the doctor.',
             imageSrc: DocTalkImage,
+            projectLink: 'https://doc-talk-1217027d6c32.herokuapp.com/',
         },
 
     ];
+    const handleCardClick = (e, projectLink) => {
+        // Prevent the default behavior of the anchor tag
+        e.preventDefault();
+
+        // Navigate to the project link
+        window.open(projectLink, '_blank');
+    };
 
     return (
         <Container>
-        <div style={{ padding: '16px', marginTop: '20px', width: '100%' }}>
-            <div className="card" style={{ padding: '16px', marginTop: '20px' }}>
-                <Grid container spacing={2}>
-                    {projects.map((project, index) => (
-                        <Grid item xs={12} sm={6} md={4} key={index}>
-                             <a href={project.projectLink} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}></a>
-                            <Card
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    justifyContent: 'space-between',
-                                    height: '100%',
-                                    '&:hover': {
-                                        '& .project-details': {
-                                            opacity: 1,
-                                            transform: 'translateY(0)',
-                                        },
-                                    },
-                                }}
-                            >
-                                <CardMedia
-                                    component="img"
-                                    src={project.imageSrc}
-                                    alt="Project"
+            <div style={{ padding: '16px', marginTop: '20px', width: '100%' }}>
+                <div className="card" style={{ padding: '16px', marginTop: '40px' }}>
+                    <Grid container spacing={2}>
+                        {projects.map((project, index) => (
+                            <Grid item xs={12} sm={6} md={4} key={index}>
+                                <Card
                                     sx={{
-                                        width: '100%',
-                                        height: '300px',
-                                        objectFit: 'cover',
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        justifyContent: 'space-between',
+                                        height: '100%',
+                                        '&:hover': {
+                                            '& .project-link-button': {
+                                                opacity: 1,
+                                                transform: 'translateY(0)',
+                                            },
+                                            '& .card-content': {
+                                                opacity: 1,
+                                                height: 'auto',
+                                            },
+                                        },
                                     }}
-                                />
-                                    <CardContent>
+                                >
+                                    <CardMedia
+                                        component="img"
+                                        src={project.imageSrc}
+                                        alt="Project"
+                                        sx={{
+                                            width: '100%',
+                                            height: '350px',
+                                            objectFit: 'cover',
+                                            cursor: 'pointer',
+                                        }}
+                                        onClick={(e) => handleCardClick(e, project.projectLink)}
+                                    />
+                                    <CardContent className="card-content" sx={{ opacity: 0, height: 0, overflow: 'hidden', transition: 'opacity 0.3s, height 0.3s' }}>
                                         <Typography variant="h6" gutterBottom>
                                             {project.title}
                                         </Typography>
                                         <Typography variant="body2">{project.description}</Typography>
                                     </CardContent>
-                                    <div className="project-details" style={{ opacity: 0, transform: 'translateY(10px)', transition: 'opacity 0.3s, transform 0.3s' }}>
-                                        <Typography variant="h6">{project.title}</Typography>
-                                        <Typography variant="body2">{project.description}</Typography>
-                                    </div>
+                                    <Button
+                                        className="project-link-button"
+                                        sx={{
+                                            opacity: 0,
+                                            transform: 'translateY(10px)',
+                                            transition: 'opacity 0.3s, transform 0.3s',
+                                            alignSelf: 'flex-end',
+                                            marginRight: '16px',
+                                        }}
+                                        onClick={(e) => handleCardClick(e, project.projectLink)}
+                                    >
+                                        Project Link
+                                    </Button>
                                 </Card>
                             </Grid>
                         ))}
